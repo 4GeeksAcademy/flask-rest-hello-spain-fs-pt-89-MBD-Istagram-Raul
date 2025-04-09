f1b1a1eb2a98ef1d9e46e7b0a6312dd7e2570437
RaulSalas23 <rast2305@gmail.com>
2025-04-09 15:55:42 +0000

    modeloBDInstagram-Raul

--- src/models.py
+++ src/models.py
@@ -1,19 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer, Date
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'usuarios'
+    id_user: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    biography: Mapped[str] = mapped_column(String(120), nullable=True)
+    creat_date: Mapped[datetime] = mapped_column(Date, nullable=False)
 
+class Publications(Base): 
+    __tablename__ = 'publicaciones'
+    id_publication: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_user: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id_user'))
+    publication: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=True)
+    publication_date: Mapped[datetime] = mapped_column(Date, nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Comments(Base): 
+    __tablename__ = 'comentarios'
+    id_comment: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_publication: Mapped[int] = mapped_column(Integer, ForeignKey('publicaciones.id_publication'))
+    id_user: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id_user'))
+    comment: Mapped[str] = mapped_column(String(120), nullable=False)
+    comment_date: Mapped[datetime] = mapped_column(Date, nullable=False)
+
+class Likes(Base): 
+    __tablename__ = 'me_gusta'
+    id_like: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_publication: Mapped[int] = mapped_column(Integer, ForeignKey('publicaciones.id_publication'))
+    id_user: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id_user'))
+    like_date: Mapped[datetime] = mapped_column(Date, nullable=False)
+
+class Followers(Base): 
+    __tablename__ = 'seguidores'
+    id_follower: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_user: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id_user'))
+    id_followed: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id_user'))
+    follow_date: Mapped[datetime] = mapped_column(Date, nullable=False)
+
+# Método para serializar información sensible
+def serialize_user(self):
+    return {
+        "id_user": self.id_user,
+        "email": self.email,
+        # No serializamos contraseñas por motivos de seguridad
+    }
